fd87104a987e638bfad45bd5bb440a9b3bf4d5a6
ZiruiWang <32398353+zirui-wang@users.noreply.github.com>
2018-10-24 14:33:14 -0700

    Animation (#1)
    * Add footer.
    
    * Add interaction in navigation icons.
    
    * Fix bug
    
    * Add Instagram
    
    * Use typed.js.

---
+++ js/typed.js
@@ -0,0 +1,10 @@
+var typed1 = new Typed('#typed1', {
+  strings: [
+    'Hello this is <em>Zirui Wang</em>. He is an <strong>Engineer</strong>.',
+    'Hello this is <em>Zirui Wang</em>. He is a <strong>Designer</strong>.',
+    'Hello this is <em>Zirui Wang</em>. He is a <strong>Dreamer</strong>.  (*>_<*)ﾉ  🍔'
+  ],
+  typeSpeed: 100,
+  backSpeed: 50,
+  smartBackspace: true
+});
