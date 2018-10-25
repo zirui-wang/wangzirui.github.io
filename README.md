bec92e2b914930dce724119513e6f14dbbea40f6
zirui-wang <zirui.zirui.wang.wang@gmail.com>
2018-10-24 22:36:38 -0700

    CNAME

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # zirui.io
+
+Just my personal website.
+
+## Libraries Used
+
+1. [typed.js](https://github.com/mattboldt/typed.js/) – By @mattboldt
+
+## License
+
+MIT
--- js/typed.js
+++ js/typed.js
@@ -1,10 +1,10 @@
 var typed1 = new Typed('#typed1', {
   strings: [
-    'Hello this is <em>Zirui Wang</em>. He is an <strong>Engineer</strong>.',
-    'Hello this is <em>Zirui Wang</em>. He is a <strong>Designer</strong>.',
-    'Hello this is <em>Zirui Wang</em>. He is a <strong>Dreamer</strong>.  (*>_<*)ﾉ  🍔'
+    'Hello this is Zirui Wang. He is an <strong>Engineer</strong>.',
+    'Hello this is Zirui Wang. He is a <strong>Designer</strong>.',
+    'Hello this is Zirui Wang. He is a <strong>Dreamer</strong>.  (*>_<*)ﾉ'
   ],
-  typeSpeed: 100,
-  backSpeed: 50,
+  typeSpeed: 40,
+  backSpeed: 20,
   smartBackspace: true
 });
